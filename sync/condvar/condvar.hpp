5f854ceaff798d60ddeb06c2e1d9a9946ed93c02
Light-creator <ibadulasha@ya.ru>
2024-12-10 10:43:30 +0300

    task condvar

--- sync/condvar/condvar.hpp
+++ sync/condvar/condvar.hpp
@@ -2,11 +2,24 @@
 
 #include <twist/ed/std/atomic.hpp>
 #include <twist/ed/wait/futex.hpp>
+#include "mutex.hpp"
+
+#include <fmt/core.h>
 
 #include "AtomicWrapper.hpp"
 
 #include <cstdint>
 #include <vector>
+
+struct cond_node_t {
+  twist::ed::std::atomic<uint32_t> flag_{0};
+  cond_node_t* next_ = nullptr;
+};
+
+struct cond_list_t {
+  cond_node_t* node_ = nullptr;
+  Mutex m_;
+};
 
 class CondVar {
 public:
@@ -14,57 +27,55 @@ public:
   // https://en.cppreference.com/w/cpp/named_req/BasicLockable
   template <class Mutex>
   void Wait(Mutex& m) {
-    // m.lock();
-    // if(holder_idx == -1) holder_idx = v_locked.size();
-    // v_locked.push_back(AtomicWrapper<uint32_t>(0));
     
-    // uint32_t expected = 0;
-    // int idx = holder_idx;
-    // while(v_locked[idx].val.compare_exchange_weak(
-    //   expected,
-    //   1,
-    //   std::memory_order_acquire
-    // )) {
-    //   expected = 0;
-    //   twist::ed::futex::Wait(v_locked[idx].val, 0);
-    // }
-    locked_.store(1);
+    lst_.m_.lock();
+      
+    cond_node_t* new_node = new cond_node_t;
+    new_node->next_ = lst_.node_;
+    lst_.node_ = new_node;
+
+    lst_.m_.unlock();
+
     m.unlock();
-    twist::ed::futex::Wait(locked_, 1);
-    locked_.fetch_sub(1, std::memory_order_relaxed);
+    
+    if(new_node) {
+      twist::ed::futex::Wait(new_node->flag_, 0);
+      delete new_node;
+    }
+       
     m.lock();
-    // while(locked_.compare_exchange_weak(
-    //   expected,
-    //   1,
-    //   std::memory_order_acquire
-    // )) {
-    //   expected = 0;
-    // }
-    // 
-    // m.unlock();
   }
 
   void NotifyOne() {
-    auto wake_key = twist::ed::futex::PrepareWake(locked_);
-    locked_.store(0);
-    twist::ed::futex::WakeAll(wake_key);
-    // auto wake_key = twist::ed::futex::PrepareWake(v_locked[holder_idx].val);
-    // v_locked[holder_idx].val.store(0, std::memory_order_release);
-    // if((int)v_locked.size() > holder_idx+1) holder_idx++;
-    // else holder_idx = -1;
-    // twist::ed::futex::WakeAll(wake_key);
+    lst_.m_.lock();
+    
+    if(lst_.node_) {
+      auto wake_key = twist::ed::futex::PrepareWake(lst_.node_->flag_);
+      lst_.node_->flag_.store(1);
+
+      lst_.node_ = lst_.node_->next_;
+
+      twist::ed::futex::WakeOne(wake_key);
+    }
+
+    lst_.m_.unlock();
   }
 
   void NotifyAll() {
-    while(true) {
-      NotifyOne();
-    } 
+    lst_.m_.lock();
+
+    while(lst_.node_) {
+      auto wake_key = twist::ed::futex::PrepareWake(lst_.node_->flag_);
+      lst_.node_->flag_.store(1);
+
+      lst_.node_ = lst_.node_->next_;
+
+      twist::ed::futex::WakeOne(wake_key);
+    }
+
+    lst_.m_.unlock();
   }
 
 private:
-  // std::vector<AtomicWrapper<uint32_t>> v_locked;
-  // int holder_idx = -1;
-  twist::ed::std::atomic<uint32_t> locked_;
-  // twist::ed::std::atomic<uint32_t> count = 0;
-  // twist::ed::std::atomic<bool> *q;
+  cond_list_t lst_;
 };
--- sync/condvar/mutex.hpp
+++ sync/condvar/mutex.hpp
@@ -2,92 +2,56 @@
 
 #include <twist/ed/std/atomic.hpp>
 #include <twist/ed/wait/futex.hpp>
-#include <twist/ed/fmt/print.hpp>
 
-#include "AtomicWrapper.hpp"
-
-#include <vector>
+#include <cstdint>
+#include <fmt/core.h>
 
 class Mutex {
-public:
-  Mutex(): locked_(0) {}
+ public: 
+  uint32_t cmpxchg(uint32_t expected, uint32_t desired) {
+    uint32_t* old = &expected;
+    locked_.compare_exchange_strong(expected, desired);
+    return *old;
+  }
 
   void Lock() {
-    uint32_t expected = 0;
-    while(!locked_.compare_exchange_weak(
-      expected,
-      1,
-      std::memory_order_acquire
-    )) {
-      expected = 0;
-      // twist::ed::futex::Wait(locked_, 0);
+    uint32_t state = cmpxchg(0, 1);
+    
+    // state != 0 -> we have to wait
+    if(state != 0) {
+      do {
+        // if we already have waiters || waiters are comming
+        if(state == 2 || cmpxchg(1, 2)) {
+          twist::ed::futex::Wait(locked_, 2);
+        }
+
+        // try to lock with waiters until we will be waken up
+      } while((state = cmpxchg(0, 2)) != 0);
     }
   }
 
   void Unlock() {
-    // auto wake_key = twist::ed::futex::PrepareWake(locked_);
-    locked_.store(0, std::memory_order_release);
-    // twist::ed::futex::WakeAll(wake_key);
+    if(locked_.fetch_sub(1) != 1) {
+      auto wake_key = twist::ed::futex::PrepareWake(locked_);
+      locked_.store(0);
+      twist::ed::futex::WakeAll(wake_key);
+
+    }
+  }
+  
+  // https://en.cppreference.com/w/cpp/named_req/BasicLockable
+
+  void lock() {  // NOLINT
+    Lock();
   }
 
-  void unlock() { Unlock(); }
-  void lock() { Lock(); }
+  void unlock() {  // NOLINT
+    Unlock();
+  }
 
-private:
-  twist::ed::std::atomic<uint32_t> locked_;
+ private:
+  // 0 - unloked
+  // 1 - loked without waiters
+  // 2 - loked with waiters
+  twist::ed::std::atomic<uint32_t> locked_{0};
 };
-// class Mutex {
-// public:
-//   Mutex() {}
-
-//   void Lock() {
-//     bool expected = false;
-
-//     int idx = v_locked.size();
-//     v_locked.push_back(AtomicWrapper<bool>(false));
-
-//     for(size_t i=0; i<v_locked.size(); i++) twist::ed::fmt::println("{}", v_locked[i].val.load());
-
-
-//     twist::ed::fmt::println("lock: {}", idx);
-//     
-//     if(holder_idx == -1) {
-//       holder_idx = idx;
-//     }
-
-//     twist::ed::fmt::println("holder_idx: {}", holder_idx);
-
-//     while(!v_locked[idx].val.compare_exchange_weak(
-//       expected, 
-//       true, 
-//       std::memory_order_acquire
-//     )) {
-//       expected = false;
-//     }
-//   }
-
-//   void Unlock() {
-//     twist::ed::fmt::println("unlock: {}", holder_idx);
-//     v_locked[holder_idx].val.store(false, std::memory_order_release);
-//     if((int)v_locked.size() > holder_idx+1) {
-//       holder_idx++;
-//     } else {
-//       holder_idx = -1;
-//     }
-//   }
-
-//   // BasicLockable
-//   // https://en.cppreference.com/w/cpp/named_req/BasicLockable
-
-//   void lock() {  // NOLINT
-//     Lock();
-//   }
-
-//   void unlock() {  // NOLINT
-//     Unlock();
-//   }
-
-// private:
-//   int holder_idx = -1;
-//   std::vector<AtomicWrapper<bool>> v_locked;
-// };
